20ca1e691792a75c58d94cfcf6561cb2e37f29af
amandeepempkasper-stack <amandeepempkasper@gmail.com>
2026-02-26 16:51:21 +0530

    feat: Implement inventory management features including product addition, retrieval, updating, and status management
    - Added inventoryController for handling product-related operations such as adding, updating, and fetching products.
    - Introduced productController to manage product details, search functionality, cart operations, and order processing.
    - Created Cart and Order models to manage user cart and order details respectively.
    - Developed product model to define product schema with necessary fields for inventory management.
    - Implemented pagination and filtering for product retrieval.
    - Added error handling and validation for various operations.

---
+++ src/models/inventory/order.ts
@@ -0,0 +1,159 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    customerId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    customerDetails: {
+      name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+      mobileNumber: {
+        type: String,
+        required: true,
+      },
+    },
+
+    expectedDeliveryDate: {
+      type: Date,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: [
+        "pending",
+        "confirmed",
+        "proccessing",
+        "delivered",
+        "returned",
+        "cancelled",
+      ],
+      default: "pending",
+      required: true,
+    },
+
+    payment: {
+      paymentMethod: {
+        type: String,
+        enum: ["cash", "online"],
+        required: true,
+      },
+      paymentStatus: {
+        type: String,
+        enum: ["pending", "paid", "failed", "refunded"],
+        default: "pending",
+        required: true,
+      },
+
+      razorpay: {
+        orderId: String,
+        paymentId: String,
+        signature: String,
+        amount: Number,
+        currency: String,
+      },
+
+      paidAt: Date,
+      refundedAt: Date,
+      refundedAmount: Number,
+      refundedReason: String,
+    },
+
+    pricing: {
+      subTotal: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+
+      shippingCharge: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+
+      gst: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+
+      finalPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+      },
+    },
+
+    orderItems: [
+      {
+        productId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+      },
+    ],
+
+    shippingAddress: {
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+      landMark: {
+        type: String,
+      },
+    },
+
+    cancelledAt: Date,
+    deliveredAt: Date,
+    returnedAt: Date,
+    returnReason: String,
+  },
+  { timestamps: true, versionKey: false },
+);
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
